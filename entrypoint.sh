27baafa241ccc96a64508db6721d7829f979e6ae
Alex Aladov <a.aladov@codefresh.io>
2019-05-11 18:05:51 +0300

    improvements 0.3

--- entrypoint.sh
+++ entrypoint.sh
@@ -17,7 +17,7 @@ export GIT_ACCESS_TOKEN=$(curl -s -H "Authorization: $CF_API_KEY" https://g.code
 export REPOSITORY_LINK=${CF_COMMIT_URL%%//*}//$GIT_ACCESS_TOKEN@$(echo $CF_COMMIT_URL | sed -e "s/[^/]*\/\/\([^@]*@\)\?\([^:/]*\).*/\2/")/$CF_REPO_OWNER/$CF_REPO_NAME.git
 set | sed '/HOSTNAME=\|HOME=\|BASH=\|BASH_VERSINFO=\|BASHOPTS=\|BASH_ALIASES=\|BASH_ARGC=\|BASH_ARGV=\|EUID=\|PPID=\|SHELLOPTS=\|UID=\|TERM=\|PATH=\|PWD=/d'  | awk '$0="export "$0' > /cf-ssh-vars
 
-echo "Copying context..."
+echo "Copying ENV..."
 rsync  -zae "ssh -p${SSH_PORT} -o LogLevel=ERROR" /cf-ssh-vars ${SSH_USERNAME}@${HOST_ADDRESS}:~/volume/
 
 echo "Starting Git Clone..."
@@ -26,3 +26,11 @@ ssh ${SSH_USERNAME}@${HOST_ADDRESS} -p ${SSH_PORT} "bash -s" < /cf-git-clone.sh
 echo "Starting remote session..."
 ssh ${SSH_USERNAME}@${HOST_ADDRESS} -p ${SSH_PORT} "source volume/cf-ssh-vars && rm -f volume/cf-git-clone.sh && rm -f volume/cf-ssh-vars && cd volume/$CF_REPO_NAME ; $@"
 
+#check for artifacts to be downloaded to a pipeline volume
+if ssh ${SSH_USERNAME}@${HOST_ADDRESS} -p ${SSH_PORT} '[ -d ~/volume/output ]'
+then
+{
+echo "Uploading artifacts to /codefresh/volume/ ..."
+rsync  -Pzare "ssh -p${SSH_PORT} -o LogLevel=ERROR"  ${SSH_USERNAME}@${HOST_ADDRESS}:~/volume/output  /codefresh/volume/
+}
+fi
